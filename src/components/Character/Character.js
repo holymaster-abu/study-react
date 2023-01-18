9c943a1e7c1e2a9432742cb242cb4518b72da86b
Yarema Kulchytskyi <kulchytskyi.yarema@gmail.com>
2023-01-18 18:56:02 +0200

    s

---
+++ src/components/Character/Character.js
@@ -0,0 +1,13 @@
+const Character = ({character}) => {
+    const  {id, name, status, species, image} = character;
+    return (
+        <div>
+            <div>id: {id}</div>
+            <div>name: {name}</div>
+            <div>status: {status}</div>
+            <div>species: {species}</div>
+            <img src={image} alt={name}/>
+        </div>
+    );
+};
+export {Character};
